3fa2e6aa7cbafce05b0b01aa019dd068930fd83a
PREM-KUMAR-D <premkumar88845@gmail.com>
2025-01-03 21:48:25 +0530

    Joi intial setup done need to refactor

--- server/src/controllers/auth-controller.js
+++ server/src/controllers/auth-controller.js
@@ -2,6 +2,8 @@ export const postLogin = async (req,res)=>{
     return res.send('this is the login route');
 }
 
-export const postRegister = async(req,res)=>{   
+export const postRegister = async(req,res)=>{  
+    
+    console.log(req.body);
     return res.send('This is the register route');
 }
--- server/src/routes/auth-routes.js
+++ server/src/routes/auth-routes.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const Joi = require('joi');
+const ExpressValidation = require('express-joi-validation');
+
 
 const authControlller = require('../controllers/auth-controller');
 
 const router = express.Router();
 
-router.post('/register',authControlller.postRegister);
-router.post('/login', authControlller.postLogin);
+const validator = ExpressValidation.createValidator({});
+
+const registerSchema = Joi.object({
+    userName: Joi.string().min(3).max(12).required(),
+    password: Joi.string().min(6).max(12).required(),
+    email: Joi.string().email().required(),
+})
+
+const loginSchema = Joi.object({
+    password: Joi.string().min(6).max(12).required(),
+    email: Joi.string().email().required(),
+})
+
+
+router.post('/register',validator.body(registerSchema),authControlller.postRegister);
+router.post('/login', validator.body(loginSchema),authControlller.postLogin);
 
 
 module.exports = router;
