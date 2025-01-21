e03a49c2546587a3bf8761822e4095c003440107
PREM-KUMAR-D <premkumar888445@gmail.com>
2025-01-21 21:14:02 +0530

    get chanel details completed

--- server/src/controllers/chanel-controller.js
+++ server/src/controllers/chanel-controller.js
@@ -1,11 +1,37 @@
-exports.getChanelDetails = (req,res,next)=>{
-    console.log(req);
+const user = require('../models/user');
+const chanel = require('../models/channel');
 
-    return res.status(200).json({
-        id : 1,
-        title : 'Chanel',
-        description: 'Dummy',
-        isOnline: false,
-        streamUrl: 'http',
-    })
+exports.getChanelDetails = async(req,res,next)=>{
+    try {
+        const { chanelId } = req.params;
+
+        const chanelDet = await chanel.findById(chanelId);
+
+        if(!chanelDet){
+            return res.status(404).send('Channel not found');
+        }
+
+        const userDet = await user.findOne({
+            chanel: chanelId
+        }, {username: 1})
+
+        const streamUrl = 'http';
+
+        const isOnline = false;
+
+
+        return res.status(200).json({
+            id : chanelDet._id,
+            title : chanelDet.title,
+            description: chanelDet.description,
+            isOnline,
+            streamUrl,
+            username: userDet.username,
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send("Something went wrong")        
+    }
+
 }
