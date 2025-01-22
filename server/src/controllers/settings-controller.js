92b0cf9122a18201c414657548baf15aff4eb54a
PREM-KUMAR-D <premkumar888445@gmail.com>
2025-01-22 11:22:17 +0530

    update settings route completed

--- server/src/controllers/settings-controller.js
+++ server/src/controllers/settings-controller.js
@@ -1,11 +1,69 @@
+const user = require('../models/user');
+const chanel = require('../models/channel');
+
 exports.getChanelSettings = async(req,res,next)=>{
     try {
         const {userId} = req.user;
 
-        console.log(userId);
+        
 
-        return res.status(200).send('this channel is secured')
+        const userData = await user.findById(userId,{
+            chanel: 1,
+            username: 1,
+        }).populate('chanel');
 
+        console.log(userData)
+
+        return res.status(200).json({
+            id: userData.chanel._id,
+            username: userData.username,
+            title: userData.chanel.title,
+            description: userData.chanel.description,
+            avatarUrl : userData.chanel.avatarUrl,
+            streamKey: userData.chanel.streamKey,
+        })
+
+
+    } catch (error) {
+        return res.status(500).send('something went wrong')
+    }
+}
+
+
+exports.putChanelSettings = async(req,res,next)=>{
+    try {
+        const {userId}  = req.user;
+
+        const { title, description, username , avatarUrl} = req.body;
+
+        const userData = await user.findById(userId,{
+            chanel: 1,
+            username: 1
+        })
+
+        if(userData.username !== username){
+            await user.updateOne({
+                _id: userId
+            },{
+                username
+            })
+        }
+
+        const chanelData  = await chanel.findByIdAndUpdate(userData.chanel, {
+            title,
+            description,
+            avatarUrl,
+            isActive: true,
+
+        },{new: true})
+
+        return res.status(200).json({
+            username,
+            title: chanelData.title,
+            description: chanelData.description,
+            avatarUrl: chanelData.avatarUrl,
+            chanelId: chanelData._id,
+        })
 
     } catch (error) {
         return res.status(500).send('something went wrong')
--- server/src/routes/setting-routes.js
+++ server/src/routes/setting-routes.js
@@ -2,13 +2,23 @@ const express = require('express')
 const ExpressValidation = require('express-joi-validation');
 const Joi = require('joi');
 const { verifyToken } = require('../middlewares/auth');
-const {getChanelSettings} = require('../controllers/settings-controller');
+const {getChanelSettings , putChanelSettings} = require('../controllers/settings-controller');
 
 const validator  = ExpressValidation.createValidator({});
 
 const router = express.Router();
 
+const chanelSettingSchema = Joi.object({
+    username: Joi.string().min(3).max(12),
+    description: Joi.string().min(10).max(200),
+    title: Joi.string().min(3).max(30),
+    avatarUrl : Joi.string().uri(),
+})
+
+
 router.get('/chanel', verifyToken,getChanelSettings);
+
+router.put('/chanel',verifyToken, validator.body(chanelSettingSchema), putChanelSettings );
 
 
 module.exports = router;
